f1b27457ef2b77de627221ced5f6abcb2e5104a3
志賀 昭駿 <a-shiga@m3c-2022mac04.local>
2022-12-20 02:00:27 +0900

    ルート

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'parents#index'
+  resources :parents, only: %i(new create index show destroy)
 end
